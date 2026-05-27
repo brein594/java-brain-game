205d3f2c794bceba3a0938585736c33b8fe4d034
brein594 <brein594@gmail.com>
2026-05-27 14:04:38 +0300

    add jar format

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
-    id("java")
+    //id("java")
+    application // заменяет id("java")
     //id("com.github.ben-manes.versions") version "0.54.0"
 }
 
+
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") } //точка входа в приложение + в plugins строка
 
 repositories {
     mavenCentral()
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,8 +2,8 @@ package hexlet.code;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class App {
     public static void main(String[] args) {
-        System.out.printf("Hello world");
+        System.out.println("Welcome to the Brain Games!");
     }
 }
