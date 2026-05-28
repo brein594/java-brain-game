a1ff0d4c3fc4fea174a117b6b3a7336546bc082e
brein594 <brein594@gmail.com>
2026-05-28 14:40:45 +0300

    add class Cli.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,12 @@
 package hexlet.code;
+import hexlet.code.Cli;
 
-import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
-
-        scanner.close();
+        Cli.userName();
     }
 }
